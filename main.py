a0d03f3bf2bbe8824ee73a5ddc711ba6b35c47a6
Madoc <plowmanm@go.oes.edu>
2023-11-07 13:12:31 -0800

    update

--- main.py
+++ main.py
@@ -13,6 +13,39 @@ screen_height = screen_info.current_h
 screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
 screen.fill((255, 255, 255))
 
+
+class Button:
+
+    def __init__(self, text, x, y, font_name, font_size, button_color, clickable):
+        self.text = text
+        self.x = x
+        self.y = y
+        self.font = pygame.font.Font(font_name, font_size)
+        self.normal_color = button_color
+        self.clickable = clickable
+        self.render_text()
+
+    def render_text(self):
+        self.text_surface = self.font.render(self.text, True, (0, 0, 0))
+        self.text_rect = self.text_surface.get_rect()
+        self.text_rect.x = self.x
+        self.text_rect.y = self.y
+
+    def draw(self, screen):
+        text_surface = self.text_surface
+        text_rect = self.text_rect
+        screen.blit(text_surface, text_rect)
+
+    def check_click(self, mouse_pos):
+        return self.text_rect.collidepoint(mouse_pos) if self.clickable else False
+    
+    def check_hover(self, mouse_pos):
+        if self.text_rect.collidepoint(mouse_pos):
+            pygame.draw.rect(screen, (0, 0, 0), self.text_rect, 1)
+        else:
+            pygame.draw.rect(screen, (255, 255, 255), self.text_rect, 1)
+
+
 def dijkstra_path(G, s):
     E = {s}
     d = {s: 0}
@@ -81,38 +114,6 @@ def plot_shortest(p: list):
     for i in range(len(p) - 1):
         pygame.draw.line(screen, (0, 0, 255), p[i], p[i + 1], 3)
 
-class Button:
-
-    def __init__(self, text, x, y, font_name, font_size, button_color, clickable):
-        self.text = text
-        self.x = x
-        self.y = y
-        self.font = pygame.font.Font(font_name, font_size)
-        self.normal_color = button_color
-        self.clickable = clickable
-        self.render_text()
-
-    def render_text(self):
-        self.text_surface = self.font.render(self.text, True, (0, 0, 0))
-        self.text_rect = self.text_surface.get_rect()
-        self.text_rect.x = self.x
-        self.text_rect.y = self.y
-
-    def draw(self, screen):
-        text_surface = self.text_surface
-        text_rect = self.text_rect
-        screen.blit(text_surface, text_rect)
-
-    def check_click(self, mouse_pos):
-        return self.text_rect.collidepoint(mouse_pos) if self.clickable else False
-    
-    def check_hover(self, mouse_pos):
-        if self.text_rect.collidepoint(mouse_pos):
-            pygame.draw.rect(screen, (0, 0, 0), self.text_rect, 1)
-        else:
-            pygame.draw.rect(screen, (255, 255, 255), self.text_rect, 1)
-
-    
 
 buttons = []
 nodes = []
@@ -210,3 +211,4 @@ while True:
             pygame.draw.line(screen, (0, 0, 0), edge[0], edge[1], 1)
 
     pygame.display.update()
+    
